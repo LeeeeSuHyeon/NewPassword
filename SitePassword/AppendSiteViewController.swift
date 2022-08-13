1c5e424c36a7505b35c50c777988c17b5d326679
LeeeeSuHyeon <tngus0673@naver.com>
2022-08-13 20:05:22 +0900

    초기설정

---
+++ SitePassword/AppendSiteViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AppendSiteViewController.swift
+//  SitePassword
+//
+//  Created by 이수현 on 2022/08/13.
+//
+
+import UIKit
+
+class AppendSiteViewController: UIViewController {
+    @IBOutlet weak var lbltitle: UILabel!
+    @IBOutlet weak var txtSiteAddress: UITextField!
+    @IBAction func btnSite(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
